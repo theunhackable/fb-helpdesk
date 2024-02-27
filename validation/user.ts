373a0565637a5bf7f57029c5e34228ae8afb46bb
theunhackable <srirangasaipagala@gmail.com>
2024-02-27 15:19:47 +0530

    user validation

--- validation/user.ts
+++ validation/user.ts
@@ -4,8 +4,9 @@ export const nameSchema = z.string().min(3);
 export const emailSchema = z.string().email();
 export const passwordSchema = z.string().min(6).max(20);
 export const userSchema = z.object({ 
+    _id: z.string().optional(),
     name: nameSchema,
     email: emailSchema,
     password: passwordSchema,
-    code: z.string().optional().nullable()
+    code: z.string().optional()
 });
